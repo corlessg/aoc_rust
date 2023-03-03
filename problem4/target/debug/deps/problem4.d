0012dc07f88d45b7ae3793f1e35b0cf9b053b606
corless_g <corlessg@gmail.com>
2023-03-02 22:43:28 -0500

    problem4

---
+++ problem4/target/debug/deps/problem4.d
@@ -0,0 +1,5 @@
+D:\Garrett\Documents\projects\aoc_rust\problem4\target\debug\deps\problem4.exe: src\main.rs
+
+D:\Garrett\Documents\projects\aoc_rust\problem4\target\debug\deps\problem4.d: src\main.rs
+
+src\main.rs:
---
+++ problem4/target/debug/problem4.d
@@ -0,0 +1,1 @@
+D:\Garrett\Documents\projects\aoc_rust\problem4\target\debug\problem4.exe: D:\Garrett\Documents\projects\aoc_rust\problem4\src\main.rs
